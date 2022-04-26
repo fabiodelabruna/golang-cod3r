8e678a30f8e6f46907898babc04d60d838ca2c05
Fabio Dela Bruna <fabio.bruna@agrotis.com>
2022-04-26 09:59:53 -0300

    commands in terminal

---
+++ fundamentals/commands/commands.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// terminal commands:
+// go env
+// go vet commands.go
+// go run commands.go
+// go get -u github.com/go-sql-driver/mysql
+
+func main() {
+	fmt.Printf("Another program in %s!", "Go")
+}
